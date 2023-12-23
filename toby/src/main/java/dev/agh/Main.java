48fdda079dc8e67a2ac867b537fc7ab05ad5cd7d
agh <gyuho0897@gmail.com>
2023-12-23 16:57:13 +0900

    1.1 초난감 DAO First Code
    데이터베이스에 접근하는 DAO 클래스를 만들고 Main 으로 테스트 하는 코드 작성 이제 이 코드를  객체지향 설계를 통해 점진적으로 개선해 나아갈  예정이다.

---
+++ toby/src/main/java/dev/agh/Main.java
@@ -0,0 +1,26 @@
+package dev.agh;
+
+import dev.agh.dao.UserDao;
+import dev.agh.domain.User;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("myName");
+        user.setPassword("test");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "등록 조회");
+    }
+}
---
+++ toby/src/main/java/dev/agh/domain/User.java
@@ -0,0 +1,14 @@
+package dev.agh.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String name;
+    String password;
+
+
+}

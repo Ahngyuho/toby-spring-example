54bf3aa46099be88d45417002c087a43f1c91bf2
agh <gyuho0897@gmail.com>
2023-12-29 07:18:45 +0900

    refactor : 전략 패턴으로 변하는 부분/ 변하지 않는 부분 분리
    변하지 않는 부분은 예외처리 부분(context) 이고 변하는 부분은 PreparedStatement(Strategy)를 사용하는 부분이다. 컨텍스트와 전략로 나누고 클라이언트는 컨텍스트가 사용할 전략을 전달한다.
    주목할 점은 설계 형태가 DI를 연상하게 한다는 점이다.

---
+++ toby/src/main/java/dev/agh/dao/userdao/StatementStrategy.java
@@ -0,0 +1,9 @@
+package dev.agh.dao.userdao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}

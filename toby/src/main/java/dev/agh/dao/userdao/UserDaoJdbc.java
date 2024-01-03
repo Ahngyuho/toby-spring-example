10c40eb75e3e0628e03b23cb8fe14459f063eb66
agh <gyuho0897@gmail.com>
2024-01-03 23:49:12 +0900

    feat : 레벨 기능 추가를 위한 도메인 추가

--- toby/src/main/java/dev/agh/dao/userdao/UserDaoJdbc.java
+++ toby/src/main/java/dev/agh/dao/userdao/UserDaoJdbc.java
@@ -1,5 +1,6 @@
 package dev.agh.dao.userdao;
 
+import dev.agh.domain.Level;
 import dev.agh.domain.User;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -7,12 +8,11 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
-import javax.sql.DataSource;
 import java.sql.*;
 import java.util.*;
 
 public class UserDaoJdbc implements UserDao {
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDaoJdbc(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -25,13 +25,16 @@ public class UserDaoJdbc implements UserDao {
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
+            user.setLevel(Level.valueOf(rs.getInt("level")));
+            user.setLogin(rs.getInt("login"));
+            user.setRecommend(rs.getInt("recommend"));
             return user;
         }
     };
 
     public void add(final User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)",
-                user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users(id, name, password,level,login,recommend) values (?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(),user.getLevel().intValue(),user.getLogin(),user.getRecommend());
     }
 
     public User get(String id) {
--- toby/src/test/java/UserDaoTest.java
+++ toby/src/test/java/UserDaoTest.java
@@ -1,5 +1,6 @@
 import dev.agh.dao.userdao.DaoFactory;
 import dev.agh.dao.userdao.UserDaoJdbc;
+import dev.agh.domain.Level;
 import dev.agh.domain.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,36 +26,26 @@ public class UserDaoTest {
         dao = context.getBean("userDao", UserDaoJdbc.class);
         dao.deleteAll();
 
-        this.user1 = new User("1", "user1", "user1");
-        this.user2 = new User("2", "user2", "user2");
-        this.user3 = new User("3", "user3", "user3");
+        this.user1 = new User("1", "user1", "user1", Level.BASIC,1,0);
+        this.user2 = new User("2", "user2", "user2",Level.SLIVER,55,10);
+        this.user3 = new User("3", "user3", "user3", Level.GOLD, 100, 40);
     }
 
     @Test
-    public void addAndGet() throws SQLException {
-
+    public void addAndGet() {
         assertThat(dao.getCount()).isEqualTo(0);
 
-        User user = new User();
-        user.setId("gyu");
-        user.setName("park");
-        user.setPassword("test");
+        dao.add(user1);
+        User getUser1 = dao.get(user1.getId());
+        checkSameUser(user1,getUser1);
 
-        dao.add(user);
-        assertThat(dao.getCount()).isEqualTo(1);
-
-        User user2 = dao.get(user.getId());
-
-        assertThat(user.getName()).isEqualTo(user2.getName());
-        assertThat(user.getPassword()).isEqualTo(user2.getPassword());
+        dao.add(user2);
+        User getUser2 = dao.get(user2.getId());
+        checkSameUser(user2,getUser2);
     }
 
     @Test
     public void count() throws SQLException{
-        User user1 = new User("1", "1", "1");
-        User user2 = new User("2", "2", "2");
-        User user3 = new User("3", "3", "3");
-
         dao.deleteAll();
         assertThat(dao.getCount()).isEqualTo(0);
 
@@ -109,5 +100,8 @@ public class UserDaoTest {
         assertThat(user1.getId()).isEqualTo(user2.getId());
         assertThat(user1.getName()).isEqualTo(user2.getName());
         assertThat(user1.getPassword()).isEqualTo(user2.getPassword());
+        assertThat(user1.getLevel()).isEqualTo(user2.getLevel());
+        assertThat(user1.getLogin()).isEqualTo(user2.getLogin());
+        assertThat(user1.getRecommend()).isEqualTo(user2.getRecommend());
     }
 }
